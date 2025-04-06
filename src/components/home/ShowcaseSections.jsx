e7d52b37f14c58493506cf922cb7642d13b967d9
Mohamed Yasir <mhdyasir132@gmail.com>
2025-04-06 13:53:11 +0530

    Your commit message

--- src/components/home/ShowcaseSections.jsx
+++ src/components/home/ShowcaseSections.jsx
@@ -7,10 +7,10 @@ import SapphireImage4 from '../../assets/images/home/desktop/bg4.gif';
 import SapphireImage5 from '../../assets/images/home/desktop/bg4.gif'; // Added fifth image
 import SapphireImage6 from '../../assets/images/home/desktop/bg4.gif'; // Added sixth image
 import SapphireImage7 from '../../assets/images/home/desktop/bg4.gif'; // Added seventh image
-import SapphireImageMobile1 from '../../assets/images/home/mobile/mbg1.gif';
-import SapphireImageMobile2 from '../../assets/images/home/mobile/mbg17.gif';
+import SapphireImageMobile1 from '../../assets/images/home/mobile/mbg16.gif';
+import SapphireImageMobile2 from '../../assets/images/home/mobile/mbg1.gif';
 import SapphireImageMobile3 from '../../assets/images/home/mobile/mbg19.gif';
-import SapphireImageMobile4 from '../../assets/images/home/mobile/mbg16.gif';
+import SapphireImageMobile4 from '../../assets/images/home/mobile/mbg17.gif';
 import SapphireImageMobile5 from '../../assets/images/home/mobile/mbg4.gif'; // Added fifth mobile image
 import SapphireImageMobile6 from '../../assets/images/home/mobile/mbg21.gif'; // Added sixth mobile image
 import SapphireImageMobile7 from '../../assets/images/home/mobile/mbg3.gif'; // Added seventh mobile image
